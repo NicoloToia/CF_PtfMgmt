25f855d4458f8b8357e90ed691ef4ec73e89e74a
andreatrdt <75367731+andreatrdt@users.noreply.github.com>
2024-12-04 10:44:40 +0100

    Update maxSharpPortfolio.m

--- maxSharpPortfolio.m
+++ maxSharpPortfolio.m
@@ -1,43 +1,22 @@
-function [P1,maxSharpeRisk_P1, maxSharpeWgt_P1, maxSharpeRet_P1, maxSharpeSR_P1] = ...
-    maxSharpPortfolio(names, mean_returns, cov_matrix, risk_free_rate,const,flag)
+function Output_structure = maxSharpPortfolio(Ptf,risk_free_rate, name_Ptf)
 
     %   Compute the efficient frontier under the standard constraints.
     %   Compute the Maximum Sharpe Ratio Portfolio
     %
     % INPUTS
-    % names: cell array of asset names
-    % mean_returns: vector of mean returns
-    % cov_matrix: covariance matrix
+    % Ptf: Portfolio data
     % risk_free_rate: risk free rate
-    % const: constraints struct
-
-
-
-    % Create a portfolio object
-    P1 = Portfolio('AssetList', names);
-    P1 = setDefaultConstraints(P1); % all weights sum to 1, no shorting, and 100% investment in risky assets
-    P1 = setAssetMoments(P1, mean_returns, cov_matrix); % set mean returns and covariance matrix
-
-    P1.AInequality = const.Aineq;
-    P1.bInequality = const.bineq;
-
-    % estimate efficient frontier, via object method and 100 P1oints
-    pwgt1 = estimateFrontier(P1, 100);
-    % estimate portfolio moments
-    [pf_risk_P1, pf_Retn_P1] = estimatePortMoments(P1, pwgt1);
-    % plot efficient frontier
-    % plot(pf_risk_P1, pf_Retn_P1)
+    % name_Ptf: name of the portfolio
 
     % find maximum Sharpe ratio portfolio (Portfolio B or D - Maximum Sharpe Ratio Portfolio)
-    maxSharpeWgt_P1 = estimateMaxSharpeRatio(P1);
-    maxSharpeRet_P1 = mean_returns' * maxSharpeWgt_P1;
-    maxSharpeRisk_P1 = sqrt(maxSharpeWgt_P1' * cov_matrix * maxSharpeWgt_P1);
-    maxSharpeSR_P1 = (maxSharpeRet_P1 - risk_free_rate) / maxSharpeRisk_P1;
+    maxSharpeWgt_Ptf = estimateMaxSharpeRatio(Ptf);
+    maxSharpeRet_Ptf = Ptf.AssetMean' * maxSharpeWgt_Ptf;
+    maxSharpeRisk_Ptf = sqrt(maxSharpeWgt_Ptf' * Ptf.AssetCovar * maxSharpeWgt_Ptf);
+    maxSharpeSR_Ptf = (maxSharpeRet_Ptf - risk_free_rate) / maxSharpeRisk_Ptf;
 
-    % Display Portfolio B or D - Maximum Sharpe Ratio Portfolio
-    % if flag == 0
-    %     print_portfolio(maxSharpeWgt_P1, names, maxSharpeRet_P1, maxSharpeRisk_P1, maxSharpeSR_P1,'Max sharpe ratio Portfolio (B)')
-    % elseif flag == 1
-    %     print_portfolio(maxSharpeWgt_P1, names, maxSharpeRet_P1, maxSharpeRisk_P1, maxSharpeSR_P1,'Max sharpe ratio Portfolio with constraints (D)')
-    % end
+    print_portfolio(maxSharpeWgt_Ptf, Ptf.AssetList, maxSharpeRet_Ptf, maxSharpeRisk_Ptf, maxSharpeSR_Ptf, name_Ptf)
+
+    Output_structure = struct('Volatility', maxSharpeRisk_Ptf, 'Weights', maxSharpeWgt_Ptf, 'Return', maxSharpeRet_Ptf, 'Sharpe_Ratio', maxSharpeSR_Ptf );
+    Output_structure.name = name_Ptf;
+    Output_structure.Ptf = Ptf;
 end
